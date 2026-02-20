6ac97e9b13107f43feb8c03afd2292c18340221e
duda <maryaeduardaandrade0104@gmail.com>
2026-02-20 09:11:41 -0300

    resultado

--- app/views/home.php
+++ app/views/home.php
@@ -3,4 +3,16 @@
 <h2>Bem-vindo ao Sistema da Copa do Mundo</h2>
 <p>Gerencie seleções, usuários, grupos, jogos e acompanhe a classificação.</p>
 
+<br><br>
+
+<div>
+    <a href="index.php?controller=resultado&action=index">
+        <button>Registrar Resultado</button>
+    </a>
+
+    <a href="index.php?controller=classificacao&action=index">
+        <button>Ver Classificação</button>
+    </a>
+</div>
+
 <?php require_once BASE_PATH . "/app/views/footer.php"; ?>
